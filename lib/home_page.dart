f193f813ec6098b6b8b78d3e1654b46f8e921e3b
koboolean <koboolean@gmail.com>
2022-07-17 20:21:16 +0900

    Firebase, FireStore Setting 및 ttf 설정관련 Commit - HjCho

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:chang_2nd_main_project/login_page.dart';
+import 'package:chang_2nd_main_project/main.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'auth_service.dart';
+
+/// 홈페이지
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController jobController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
+    final user = authService.currentUser()!;
+    return Consumer(
+      builder: (context, bucketService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("TEST"),
+            actions: [
+              TextButton(
+                child: Text(
+                  "로그아웃",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  // 로그아웃
+                  context.read<AuthService>().signOut();
+
+                  // 로그인 페이지로 이동
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
