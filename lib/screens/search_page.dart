cf852d3f4b480d84666e89cfcdaf6b56e660a2af
johyeonjun <koboolean@gmail.com>
2022-09-10 15:34:58 +0900

    Search Page 수정

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -383,11 +383,11 @@ class _MySearchState extends State<MySearch>
                                   builder: (context, snapshot) {
                                     final docs =
                                         snapshot.data?.docs ?? []; // 문서들 가져오기
-                                    if (docs.isEmpty) {
-                                      return showUndfinedValue();
-                                    } else {
-                                      return selectSearchValue(docs, "food");
-                                    }
+                                    return Column(children: [
+                                      docs.isEmpty ?
+                                      showUndfinedValue() :
+                                      selectSearchValue(docs, "food")
+                                    ],);
                                   },
                                 );
                               }),
@@ -403,11 +403,12 @@ class _MySearchState extends State<MySearch>
                                   builder: (context, snapshot) {
                                     final docs =
                                         snapshot.data?.docs ?? []; // 문서들 가져오기
-                                    if (docs.isEmpty) {
-                                      return showUndfinedValue();
-                                    } else {
-                                      return selectSearchValue(docs, "lodge");
-                                    }
+                                    return Column(children: [
+                                        docs.isEmpty ?
+                                          showUndfinedValue() :
+                                          selectSearchValue(docs, "lodge")
+                                    ],);
+
                                   },
                                 );
                               }),
@@ -423,11 +424,11 @@ class _MySearchState extends State<MySearch>
                                   builder: (context, snapshot) {
                                     final docs =
                                         snapshot.data?.docs ?? []; // 문서들 가져오기
-                                    if (docs.isEmpty) {
-                                      return showUndfinedValue();
-                                    } else {
-                                      return selectSearchValue(docs, "place");
-                                    }
+                                    return Column(children: [
+                                      docs.isEmpty ?
+                                      showUndfinedValue() :
+                                      selectSearchValue(docs, "place")
+                                    ],);
                                   },
                                 );
                               }),
