fb454b2d3d81e87851069644341e5d6c77221749
DohyeonGwang <107793665+DohyeonGwang@users.noreply.github.com>
2022-10-04 23:48:18 +0900

    foodDescription 추가

--- lib/model/foodToSend.dart
+++ lib/model/foodToSend.dart
@@ -4,6 +4,7 @@ class FoodToSend {
   final String? foodArea;
   final String? foodBusinessHours;
   final String? foodClassification;
+  final String? foodDescription;
   final String? foodField14;
   final String? foodIdx;
   final String? foodName;
@@ -15,20 +16,22 @@ class FoodToSend {
   final String? foodUrl1;
   final String? foodUrl2;
 
-  const FoodToSend(
-      {this.foodSubtitle,
-      this.foodAddress,
-      this.foodArea,
-      this.foodBusinessHours,
-      this.foodClassification,
-      this.foodField14,
-      this.foodIdx,
-      this.foodName,
-      this.foodNaverLink,
-      this.foodNote,
-      this.foodPhonenumber,
-      this.foodPrice,
-      this.foodServingSize,
-      this.foodUrl1,
-      this.foodUrl2});
+  const FoodToSend({
+    this.foodSubtitle,
+    this.foodAddress,
+    this.foodArea,
+    this.foodBusinessHours,
+    this.foodClassification,
+    this.foodDescription,
+    this.foodField14,
+    this.foodIdx,
+    this.foodName,
+    this.foodNaverLink,
+    this.foodNote,
+    this.foodPhonenumber,
+    this.foodPrice,
+    this.foodServingSize,
+    this.foodUrl1,
+    this.foodUrl2,
+  });
 }
